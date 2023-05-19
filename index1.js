cb50da1f9ee9b1748173dc99dff9cca463143cf7
KAILASHCOOL <kailash100singh@gmail.com>
2023-05-19 17:59:51 +0530

    basic calculator

---
+++ index1.js
@@ -0,0 +1,21 @@
+function hello(){
+    let a=parseInt(prompt("Enter the no"))
+    let b=parseInt(prompt("Enter the no"))
+    document.write(a+b,"<br>");
+
+}
+function yo(){
+    let a=parseInt(prompt("Enter the no"))
+    let b=parseInt(prompt("Enter the no"))
+    document.write(a*b ,"<br>");
+}
+function pop(){
+    let a=parseInt(prompt("Enter the no"))
+    let b=parseInt(prompt("Enter the no"))
+    document.write(a%b,"<br>");
+}
+function lol(){
+    let a=parseInt(prompt("Enter the no"))
+    let b=parseInt(prompt("Enter the no"))
+    document.write(a/b,"<br>");
+}
